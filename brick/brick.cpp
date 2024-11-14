a2f286a860f99369f4697b827e6442e7c9faa8aa
korolyam <worldofcrew1@gmail.com>
2024-11-14 19:05:35 +0200

    Добавьте файлы проекта.

---
+++ brick/brick.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    double ba, bb, bc, ga, gb;
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Программа для вычисления пройдёт ли кирпич (3 измерения) через прямоугольное отверстие (2 измерения)\n";
+    std::cout << "Введите длину, ширину и длину кирпича соответственно\n";
+    std::cin >> ba;
+    std::cin >> bb;
+    std::cin >> bc;
+    std::cout << "Введите длину и ширину отверстия соответственно\n";
+    std::cin >> ga;
+    std::cin >> gb;
+    if (ba * bb <= ga * gb || ba * bc <= ga * gb || bb * bc <= ga * gb)
+    {
+        std::cout << "Пройдёт\n";
+    }
+    else
+    {
+        std::cout << "Не пройдёт\n";
+    }
+    std::cout << "Повторить (1 - да, 0 - нет)\n";
+    std::cin >> ba;
+    if (ba == 1)
+    {
+        main();
+    }
+    return 0;
+}
+
