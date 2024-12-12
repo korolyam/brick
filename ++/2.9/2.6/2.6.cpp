24983bef9fc309abeeaf36cf8d9e4519acdb9e4d
korolyam <70631951+korolyam@users.noreply.github.com>
2024-12-12 19:30:10 +0200

    Add files via upload

---
+++ ++/2.9/2.6/2.6.cpp
@@ -0,0 +1,48 @@
+﻿#include <cstdint>
+#include <limits>
+#include <cstdlib>
+#include <iostream>
+#include <locale.h>
+void ThreeAvg()
+{
+    int n;
+    std::cout << "Введите число\n";
+    std::cin >> n;
+    if (std::cin.fail())
+    {
+        std::cout << "Было введено недопустимое число";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return;
+    }
+    for (int i = 1; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int v = 0; v < 10; v++)
+            {
+                if (i + j + v < n)
+                {
+                    std::cout << i * 100 + j * 10 + v << " ";
+                }
+            }
+        }
+    }
+    std::cout << "\n";
+    return;
+}
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+    do {
+        ThreeAvg();
+        std::cout << "\nЕщё раз? (1 - да, 0 - нет): ";
+        int answer;
+        std::cin >> answer;
+        if (answer != 1)
+        {
+            break;
+        }
+    } while (true);
+    return 0;
+}
---
+++ ++/2.9/2.9/2.9.cpp
@@ -0,0 +1,59 @@
+﻿#include <cstdint>
+#include <limits>
+#include <cstdlib>
+#include <iostream>
+#include <locale.h>
+void ThreeAvg()
+{
+    int n, temp, i = 0;
+    int dig[10]{ 9,8,7,6,5,4,3,2,1,0 };
+    std::cout << "Введите число\n";
+    std::cin >> n;
+    if (std::cin.fail())
+    {
+        std::cout << "Было введено недопустимое число";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return;
+    }
+    do
+    {
+        if ((pow(10, i + 1) < n))
+        {
+            temp = i > 0 ? fmod(n / pow(10, i), 10) : fmod(n, 10);
+        }
+        else
+        {
+            temp = n / pow(10, i);
+                dig[9 - temp] = -10;
+                break;
+        }
+        dig[9 - temp] = -10;
+            ++i;
+    }
+    while (true);
+    for (int j = 0; j < 10; j++)
+    {
+        if (dig[j] != -10)
+        {
+            std::cout << dig[j] << " ";
+        }
+    }
+    std::cout << "\n";
+    return;
+}
+int main()
+{
+    std::setlocale(LC_ALL, "Russian");
+    do {
+        ThreeAvg();
+        std::cout << "\nЕщё раз? (1 - да, 0 - нет): ";
+        int answer;
+        std::cin >> answer;
+        if (answer != 1)
+        {
+            break;
+        }
+    } while (true);
+    return 0;
+}
