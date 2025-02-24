05ca11dba8bdb17a502f92d9cc7957221453b885
korolyam <70631951+korolyam@users.noreply.github.com>
2025-02-24 20:29:18 +0200

    Add files via upload

---
+++ ++/8/8.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <cmath>
+void program1()
+{
+    std::cout << "Программа для нахождения точки входа элемента в массив и его количества повторений.\n";
+    bool flag = false;
+    const int n = 10;
+    int array[n];
+    for (int i = 0; i < n; ++i)
+    {
+        array[i] = rand() % 10 + 1;
+        std::cout << array[i] << " ";
+    }
+    std::cout << "\n";
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i; j >= 0; --j)
+        {
+            if (array[i] == array[j] && i != j)
+            {
+                flag = true;
+            }
+        }
+        if (!flag)
+        {
+            int k = 0;
+            for (int j = 0; j < n; ++j)
+            {
+                if (array[j] == array[i])
+                {
+                    k++;
+                }
+            }
+            std::cout << array[i] << " " << k << " " << i << "\n";
+        }
+        flag = false;
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    bool answer;
+
+    program1();
+
+    std::cout << "\nЕщё раз? (1 - yes, 0 - no): ";
+    std::cin >> answer;
+
+    if (answer)
+    {
+        main();
+    }
+    return 0;
+}
---
+++ ++/9/9.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <ranges>
+#include <array>
+#include <functional>
+#include <iomanip>
+class I
+{
+    void program1()
+    {
+        std::cout << "Программа для сортировки массива из 10 случ. элементов двумя методами.\n";
+        bool flag = false;
+        const int n = 10;
+        int array[n];
+        std::cout << "Первоначальный массив\n";
+        for (int i = 0; i < n; ++i)
+        {
+            array[i] = rand() % 10 + 1;
+            std::cout << array[i] << " ";
+        }
+        std::cout << "\n";
+        std::sort(array[0], array[n - 1]);
+        std::cout << "Отсортированный массив\n";
+        for (int i = 0; i < n; ++i)
+        {
+            array[i] = rand() % 10 + 1;
+            std::cout << array[i] << " ";
+        }
+        std::cout << "\n";
+        std::cout << "Первоначальный массив\n";
+        for (int i = 0; i < n; ++i)
+        {
+            array[i] = rand() % 10 + 1;
+            std::cout << array[i] << " ";
+        }
+        std::cout << "\n";
+        b(array);
+        std::cout << "Отсортированный массив\n";
+        for (int i = 0; i < n; ++i)
+        {
+            array[i] = rand() % 10 + 1;
+            std::cout << array[i] << " ";
+        }
+        std::cout << "\n";
+    }
+    int main()
+    {
+        setlocale(LC_ALL, "Russian");
+        bool answer;
+        program1();
+        std::cout << "\nЕщё раз? (1 - yes, 0 - no): ";
+        std::cin >> answer;
+
+        if (answer)
+        {
+            main();
+        }
+        return 0;
+    }
+    void b(int vec[])
+    {
+        bool swapped;
+        for (int i = 0; i < sizeof(vec); ++i)
+        {
+            swapped = false;
+            for (int j = 0; j < sizeof(vec) - i - 1; ++j)
+            {
+                if (vec[j] > vec[j + 1])
+                {
+                    std::swap(vec[j], vec[j + 1]);
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+            {
+                break;
+            }
+        }
+    }
+};
