736fe0b15df8b3bd8ca03d23828f90881be5efd8
korolyam <70631951+korolyam@users.noreply.github.com>
2025-03-25 12:49:39 +0200

    Add files via upload

---
+++ ++(3)/3.16/3.16.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cmath>
+#include <array>
+#include <iomanip>
+#include <algorithm>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    bool answer;
+    const int n = 5;  
+    const int m = 7;
+    int array2[n][m];
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            array2[i][j] = rand() % 9 + 1;
+            std::cout << array2[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            if (i == 0 || i == n - 1 || j == 0 || j == m - 1)
+            {
+                std::cout << array2[i][j] << " ";
+            }
+            else
+            {
+                std::cout << "  ";
+            }
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\nЕщё раз? (1 - yes, 0 - no): ";
+    std::cin >> answer;
+    if (answer)
+    {
+        main();
+    }
+    return 0;
+}
---
+++ ++(3)/3.17/3.17.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+#include <array>
+#include <iomanip>
+#include <algorithm>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    bool answer;
+    const int n = 6;
+    int array2[n][n];
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            array2[i][j] = 0;
+            if (i == j)
+            {
+                array2[i][j] = i + 1;
+            }
+            if (i == n - 1 - j)
+            {
+                array2[i][j] = j + 1;
+            }
+            std::cout << array2[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\nЕщё раз? (1 - yes, 0 - no): ";
+    std::cin >> answer;
+    if (answer)
+    {
+        main();
+    }
+    return 0;
+}
+
